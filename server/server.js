042c986cc2544d122a8f74398d020a6b5406fb38
burakbehlull <burakyabgu@gmail.com>
2024-01-08 14:49:26 +0300

    chore: Created .env variable

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import pageRoute from './routers/pageRoute.js'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use('/', pageRoute)
 
-app.listen(80, ()=>{
-	console.log('80 portunda çalışıyor.')
+app.listen(process.env.PORT, ()=>{
+	console.log(`${process.env.PORT} portunda çalışıyor.`)
 })
