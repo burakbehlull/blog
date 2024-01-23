baddff289ff1df7bf3e117153e65b17827dcffe5
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2024-01-23 19:01:29 +0300

    feat(backend): Add sorting by title and category functionality for getPosts function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,5 @@
 import Post from '../models/Post.js'
+import Category from '../models/Category.js'
 
 async function createPost(req,res){
     const post = await Post.create(req.body)
@@ -8,11 +9,47 @@ async function createPost(req,res){
 }
 
 async function getPosts(req,res){
-    const posts = await Post.find({})
-    res.json({
-        posts
-    })
-}
+    try {
+
+        const categorySlug = req.query.category;
+                const { search } = req.query;
+
+                const category = await Category.findOne({ name: categorySlug });
+
+                let filter = {};
+                
+                      if (category) {
+                        filter.category = category._id;
+                      }
+                
+                      if (search) {
+                        filter.title = { $regex: '.*' + search + '.*', $options: 'i' };
+                      }
+
+                      if (!categorySlug && !search)
+                      {
+                        filter.title = null;
+                        filter.category = null;
+                      }
+        
+                const posts = await Post.find(filter)
+                .sort('-createdAt');
+        
+        if (posts.  length) {
+            res.status(200).json(posts);
+        } else {
+            res.status(404).json({
+                message: 'No post found.'
+            });
+        }
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({
+                    error: true,
+                    message: error.message
+                });
+            }
+        }
 
 export {
     createPost, getPosts
