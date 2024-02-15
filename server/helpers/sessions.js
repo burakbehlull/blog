f3960593a46de67ff265b75e3cef24d0028a640d
burakbehlull <burakyabgu@gmail.com>
2024-02-15 23:03:03 +0300

    feat: Completed verification process

--- server/helpers/sessions.js
+++ server/helpers/sessions.js
@@ -4,7 +4,23 @@ function generateAccessToken(data){
     return jwt.sign(data, process.env.JWT_KEY, {expiresIn: '1h'})
 }
 function generateRefreshToken(data){
-    return jwt.sign(data, process.env.JWT_KEY, {expiresIn: '12h'})
+    jwt.verify(data, process.env.JWT_KEY, (err, user)=>{
+        if(err){
+            return {message: 'Token geçersiz.'}
+        }
+        
+        return jwt.sign(user, process.env.JWT_KEY, {expiresIn: '12h'})
+        
+    })
+}
+
+function verifyToken(token){
+    return jwt.verify(token, process.env.JWT_KEY, (err, user)=>{
+        if(err){
+            return {message: 'Token geçersiz.'}
+        }
+        return user
+    })
 }
 
 export {
